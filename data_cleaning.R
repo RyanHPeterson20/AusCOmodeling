0544557565145d0a3ccb3bd171ff0ef53df44692
Ryan Peterson <rhpeterson@mines.edu>
2025-11-02 22:54:05 -0700

    Update to main

--- data_cleaning.R
+++ data_cleaning.R
@@ -1,9 +1,22 @@
-
 #base data cleaning for Aus Wildfire CO-climate modeling
+
+#creates .csv and .rda files for external and internal use.
 
 #notes: 
 ## for data cleaning and prep 
 
+#libraries
+suppressMessages(library( lubridate)) #for basic date management
+
 #TODO: import from ~/Data
+#import as raw data 
+setwd("~/CO_AUS/AusCOmodeling") 
+
+
+#response data: MOPITT V9J WEDCEN
+
+
+#TODO: load in functions as needed (also move any internal functions over)
+
 
 
