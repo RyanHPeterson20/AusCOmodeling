370df9f0976cc022cd2f45b16c02a1c24d660902
Ryan Peterson <rhpeterson@mines.edu>
2025-11-17 00:39:02 -0700

    update

--- Figures/ts_figures.R
+++ Figures/ts_figures.R
@@ -179,19 +179,108 @@ dev.off()
 #perform for "base" and standardized response data
 
 #base: using NE.anom and SE.anom
+#TODO: remove when done
 range(NE.anom)
 range(SE.anom)
 
 #TODO: get this working
 #get y.ticks and labels (set-up by 10s)
-y.tick.max <- max(round(range(NE.anom.std)))
-y.ticks <- seq(-y.tick.max, y.tick.max, by = 1)
+y.NE.max <- round(max(NE.anom)/10)*10
+y.SE.max <-  round(max(SE.anom)/10)*10
 
-#par(mfrow = c(2, 1))
+y.NE.min <- round(min(NE.anom)/10)*10
+y.SE.min <- round(min(SE.anom)/10)*10
+
+y.NE.ticks <- seq(y.NE.min, y.NE.max, by = 10)
+y.SE.ticks <- seq(y.SE.min, y.SE.max, by = 10)
+
+#get envelope setup
+over.NE <- NE.anom >= 0
+NE.top <- NE.anom
+NE.top[!over.NE] <- 0
+NE.bot <- NE.anom
+NE.bot[over.NE] <-0
+
+over.SE <- SE.anom >= 0
+SE.top <- SE.anom
+SE.top[!over.SE] <- 0
+SE.bot <- SE.anom
+SE.bot[over.SE] <-0
 
 
+#response figure output:
+setwd("~/CO_AUS/AusCOmodeling/Figures")
 
+png(filename = "response_ts.png", width = 4800, height = 2400, res = 250)
+par(mfrow = c(2, 1))
+par(mar = c(0,4,0,0), oma = c(1.5,2,0,0), mgp = c(4,1,0))
 
+#NE Aus plot
+plot(time.plot, NE.anom, type = "l", col = "black", lwd = 2,
+     xaxt = "n", xlab = "",
+     yaxt = "n", ylab = "CO Anomaly [ppb]", col.lab = "black",
+     xlim = c(as.Date(resp.time.range[1]) + months(7), as.Date(resp.time.range[2]) - months(7)),
+     ylim = range(y.NE.ticks), bty = "n", cex.lab = 1.75,  xpd = NA)
+axis(side = 2, at = y.NE.ticks, cex.axis = 1.67, 
+    col = NA, line = 0.5,
+    col.ticks = "black", col.axis = "black", las =1)
+abline(v = x.ticks[1:(length(x.ticks))],
+       lty = 2, col = "grey", lwd = 2)
+abline(h = 0, lty = 1, col = "grey", lwd = 1)
+envelopePlot(x1 = time.plot,
+             y1 = NE.top,
+             x2 = time.plot,
+             y2 = rep(0, length(NE.top)),
+             col = alpha(top.col, 0.67),
+             lineCol = NA)
+envelopePlot(x1 = time.plot,
+             y1 = NE.bot,
+             x2 = time.plot,
+             y2 = rep(0, length(NE.bot)),
+             col = alpha(bot.col, 0.67),
+             lineCol = NA)
+legend(x = c(ymd("2000-11-01"), ymd("2002-01-01")),
+       y = c(45,40),
+       legend = "NE Aus",
+       box.col = "white", bg = "white",
+       xpd = NA, text.col = "grey40", cex = 2.5)
+
+#SE Aus plot
+par(mar = c(0,4,0.5,0))
+plot(time.plot, SE.anom, type = "l", col = "black", lwd = 2,
+     xaxt = "n", xlab = "",
+     yaxt = "n", ylab = "CO Anomaly [ppb]", col.lab = "black",
+     xlim = c(as.Date(resp.time.range[1]) + months(7), as.Date(resp.time.range[2]) - months(7)),
+     ylim = range(y.SE.ticks), bty = "n", cex.lab = 1.75,  xpd = NA)
+axis(side = 2, at = y.NE.ticks, cex.axis = 1.67, 
+     col = NA, line = 0.5,
+     col.ticks = "black", col.axis = "black", las =1)
+abline(v = x.ticks[1:(length(x.ticks))],
+       lty = 2, col = "grey", lwd = 2)
+abline(h = 0, lty = 1, col = "grey", lwd = 1)
+envelopePlot(x1 = time.plot,
+             y1 = SE.top,
+             x2 = time.plot,
+             y2 = rep(0, length(SE.top)),
+             col = alpha(top.col, 0.67),
+             lineCol = NA)
+envelopePlot(x1 = time.plot,
+             y1 = SE.bot,
+             x2 = time.plot,
+             y2 = rep(0, length(SE.bot)),
+             col = alpha(bot.col, 0.67),
+             lineCol = NA)
+#full plot
+text(x = x.ticks.reduced + months(6),
+     y = range(y.SE.ticks)[1]-3,
+     labels = year(x.ticks.reduced),
+     cex = 1.67, col = "black", xpd = NA)
+legend(x = c(ymd("2000-11-01"), ymd("2002-01-01")),
+       y = c(45,40),
+       legend = "SE Aus",
+       box.col = "white", bg = "white",
+       xpd = NA, text.col = "grey40", cex = 2.5)
+dev.off()
 
 
 
