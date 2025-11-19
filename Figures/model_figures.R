06aedfc4f62418f172d4a3be0f74c3e82970891e
Ryan Peterson <rhpeterson@mines.edu>
2025-11-18 17:08:07 -0700

    update

--- Figures/model_figures.R
+++ Figures/model_figures.R
@@ -1,5 +1,218 @@
 
 #model figures
 
+#figure order:
+#1. prediction (2019-2020)
+#2. Coefficient/interaction plots
+
+#libraries
 
 
+
+#data import
+setwd("~/CO_AUS/AusCOmodeling") 
+load("Data/base_RAMPmodels.rda") #"base" model
+load("Data/loyo_models.rda") #leave one year out models/refits
+
+
+#model review
+#NE Aus Early (Group 1)
+summary(NE1.lm)
+summary(NE.const.LM$`2019-2020`[[1]])
+summary(NE.vary.LM$`2019-2020`[[1]])
+
+#NE Aus Middle (Group 2)
+summary(NE2.lm)
+summary(NE.const.LM$`2019-2020`[[2]])
+summary(NE.vary.LM$`2019-2020`[[2]])
+
+#NE Aus Late (Group 3)
+summary(NE3.lm)
+summary(NE.const.LM$`2019-2020`[[3]])
+summary(NE.vary.LM$`2019-2020`[[3]])
+
+
+#SE Aus Early (Group 1)
+summary(SE1.lm)
+summary(SE.const.LM$`2019-2020`[[1]])
+summary(SE.vary.LM$`2019-2020`[[1]])
+
+#SE Aus Middle (Group 2)
+summary(SE2.lm)
+summary(SE.const.LM$`2019-2020`[[2]])
+summary(SE.vary.LM$`2019-2020`[[2]])
+
+#SE Aus Late (Group 3)
+summary(SE3.lm)
+summary(SE.const.LM$`2019-2020`[[3]])
+summary(SE.vary.LM$`2019-2020`[[3]])
+
+## --- Model Predictions --- ##
+
+
+
+
+## ---- Coeff/Interaction Figures ---- ##
+## setup
+SE2.coef <- coef(SE2.lm)
+SE2.constcoef <- coef(SE.const.LM$`2019-2020`[[2]])
+SE2.varycoef <- coef(SE.vary.LM$`2019-2020`[[2]])
+
+
+
+
+## SE Aus Middle (Group 2)
+SE2.coef
+SE2.constcoef
+SE2.varycoef
+
+layout(matrix(c(1, 6,
+                2, 6,
+                3, 6,
+                4, 6,
+                5, 6), ncol = 2, byrow = TRUE),
+       widths = c(1.5, 1), heights = c(1, 1, 1, 1, 1))
+
+par(oma = c(1, 1, 3, 9))
+
+# Store links
+links <- list()
+
+# --- Data Setup --- #
+## Nino
+SE2_ninolag <- c(40)
+SE2_ninocoef <- SE2.coef[2]
+SE22_ninolag <- c(40)
+SE22_ninocoef <- SE2.constcoef[2]
+
+## WTIO
+SE2_wtiolag <- c(14,46)
+SE2_wtiocoef <- SE2.coef[c(3:4)]
+SE22_wtiolag <- c(14,46)
+SE22_wtiocoef <- SE2.constcoef[c(3:4)]
+SE23_wtiolag <- c(14)
+SE23_wtiocoef <- SE2.varycoef[2]
+
+## ETIO
+SE2_etiolag <- c(7, 33)
+SE2_etiocoef <- SE2.coef[c(5,6)]
+SE22_etiolag <- c(7, 33)
+SE22_etiocoef <- SE2.constcoef[c(5,6)]
+SE23_etiolag  <- c(8)
+SE23_etiocoef <- SE2.varycoef[5]
+
+## TSA
+SE2_tsalag <- c(29)
+SE2_tsacoef <- SE2.coef[7]
+SE22_tsalag <- c(29)
+SE22_tsacoef <- SE2.constcoef[7]
+SE23_tsalag <- c(31,34)
+SE23_tsacoef <- SE2.varycoef[c(6,3)]
+
+## SAM (AAO)
+SE2_aaolag <- c(9, 21)
+SE2_aaocoef <- SE2.coef[c(8,9)]
+SE22_aaolag <- c(9, 21)
+SE22_aaocoef <- SE2.constcoef[c(8,9)]
+SE23_aaolag <- c(42)
+SE23_aaocoef <- SE2.varycoef[4]
+
+## OLR
+#NA
+
+# --- Range ---
+SEAus2_absmax <- max(abs(range(SE2.coef,
+                               SE2.constcoef,
+                               SE2.varycoef)))
+SEAus2_range <- c(-SEAus2_absmax, SEAus2_absmax)
+
+
+#TODO: delete when done; temp row
+par(mfrow = c(4, 1))
+
+# --- Plot 1: Nino ---
+par(mar = c(4, 4, 2, 1))
+
+plot(SE2_ninolag, SE2_ninocoef, pch = 22, 
+     col = "grey4", bg =  alpha("green4",.95), cex = 1.5,
+     xlim = c(1,52), 
+     ylim = SEAus2_range,
+     xlab = "", ylab = "")
+points(SE22_ninolag, SE22_ninocoef, pch = 22, col = "black",
+       bg =  alpha("chartreuse2",.65) , cex = 1.5)
+abline(h = 0, lty = 2)
+title("Nino", adj = 0)
+
+## --- Nino Interaction
+## nino_lag40:etio_lag7
+## base 
+## const
+
+# --- Plot 2: WTIO & ETIO ---
+par(mar = c(4, 4, 2, 1))
+
+plot(SE2_wtiolag-0.125, SE2_wtiocoef, pch = 22, col = "black",
+     bg =  alpha("magenta4",.95) , cex = 1.5, 
+     xlim = c(1,52), 
+     ylim = SEAus2_range,
+     xlab = "", ylab = "")
+points(SE22_wtiolag, SE22_wtiocoef, pch = 22, col = "black",
+       bg =  alpha("palevioletred2",.65) , cex = 1.5)
+points(SE23_wtiolag+0.125, SE23_wtiocoef, pch = 24, col = "black",
+       bg =  alpha("palevioletred2",.65) , cex = 1.33)
+points(SE2_etiolag, SE2_etiocoef, pch = 22, 
+       col = "grey4", bg =  alpha("royalblue4",.95), cex = 1.5)
+points(SE22_etiolag, SE22_etiocoef, pch = 22, col = "black",
+       bg =  alpha("royalblue2",.65) , cex = 1.5)
+points(SE23_etiolag, SE23_etiocoef, pch = 24, col = "black",
+       bg =  alpha("royalblue2",.65) , cex = 1.33)
+abline(h = 0, lty = 2)
+title("WTIO & ETIO", adj = 0)
+
+## --- ETIO Interaction
+## nino_lag40:etio_lag7
+## base
+## const
+
+## I(etio_lag8^2) 
+## vary
+## etio_lag8:tsa_lag31
+## vary
+
+
+# --- Plot 3: TSA ---
+par(mar = c(4, 4, 2, 1))
+
+plot(SE2_tsalag-0.33, SE2_tsacoef, pch = 22, col = "black",
+     bg =  alpha("darkorange3", 0.95), xlim = c(1,52), cex = 1.4,
+     ylim = SEAus2_range,
+     xlab = "", ylab = "Coefficients", cex.lab = 1.33)
+points(SE22_tsalag+0.33, SE22_tsacoef, pch = 22, col = "black",
+       bg =  alpha("darkgoldenrod2",.55) , cex = 1.5)
+points(SE23_tsalag, SE23_tsacoef, pch = 24, col = "black",
+       bg =  alpha("darkgoldenrod2",.65) , cex = 1.33)
+abline(h = 0, lty = 2)
+title("TSA", adj = 0)
+
+## --- TSA Interaction
+## etio_lag8:tsa_lag31
+## vary
+
+
+# --- Plot 4: SAM/AAO ---
+par(mar = c(4, 4, 2, 1))
+plot(SE2_aaolag-0.33, SE2_aaocoef, pch = 22,
+     col = "grey4",
+     bg =  alpha("red3",.95), cex = 1.5,
+     xlim = c(1,52), 
+     ylim = SEAus2_range,
+     xlab = "Lag", ylab = "")
+points(SE22_aaolag+0.33, SE22_aaocoef, pch = 22, col = "black",
+       bg =  alpha("coral2",.55) , cex = 1.5)
+points(SE23_aaolag, SE23_aaocoef, pch = 24, col = "black",
+       bg =  alpha("coral2",.65) , cex = 1.33)
+abline(h = 0, lty = 2)
+title("SAM (AAO)", adj = 0)
+
+
+#dev.off()
--- Modeling/lasso_models.R
+++ Modeling/lasso_models.R
@@ -174,9 +174,301 @@ names(lm.data.3)[1] <- "co"
 
 SE3.lm <- lm(formula(SE3.refit), lm.data.3)
 
-summary(SE3.lm)
+#summary(SE3.lm)
 
 #save models
+#base model fit output
+NEmodels <- list(NE1.lm, NE2.lm, NE3.lm)
+NErefits <- list(NE1.refit, NE2.refit, NE3.refit)
+SEmodels <- list(SE1.lm, SE2.lm, SE3.lm)
+SErefits <- list(SE1.refit, SE2.refit, SE3.refit)
+setwd("~/CO_AUS/AusCOmodeling/Data") 
+save(NEmodels, SEmodels, 
+     NErefits, SErefits, file = "base_RAMPmodels.rda")
 
+
+#Models without OLR
+
+#NE Aus Early (Group 1)
+y.1 <- as.numeric(NE.resp[[1]]) #co response
+X.1 <- cbind(as.matrix(NE.pred[[1]][ ,c(1:52, 105:312)])) #preds without DMI and OLR
+
+NE1.ramp.noOLR <- RAMP(X = X.1, y = y.1,
+                       penalty = "LASSO",
+                       tune = "BIC",
+                       n.lambda = 500)
+
+NE1.refit.noOLR <- refit_ramp(NE1.ramp.noOLR, X.1)
+
+#lm refit
+lm.data.1 <- as.data.frame(cbind(y.1, X.1))
+names(lm.data.1)[1] <- "co"
+
+NE1.lm.noOLR <- lm(formula(NE1.refit.noOLR), lm.data.1)
+
+summary(NE1.lm.noOLR)
+
+#NE Aus Middle (Group 2)
+y.2 <- as.numeric(NE.resp[[2]]) #co response
+X.2 <- cbind(as.matrix(NE.pred[[2]][ ,c(1:52, 105:312)])) #preds without DMI and OLR
+
+NE2.ramp.noOLR <- RAMP(X = X.2, y = y.2,
+                       penalty = "LASSO",
+                       tune = "BIC",
+                       n.lambda = 500)
+
+NE2.refit.noOLR <- refit_ramp(NE2.ramp.noOLR, X.2)
+
+#lm refit
+lm.data.2 <- as.data.frame(cbind(y.2, X.2))
+names(lm.data.2)[1] <- "co"
+
+NE2.lm.noOLR <- lm(formula(NE2.refit.noOLR), lm.data.2)
+
+summary(NE2.lm.noOLR)
+summary(NE2.lm.noOLR)$adj.r.squared
+
+
+#NE Aus Late (Group 3)
+y.3 <- as.numeric(NE.resp[[3]]) #co response
+X.3 <- cbind(as.matrix(NE.pred[[3]][ ,c(1:52, 105:312)])) #preds without DMI and OLR
+
+NE3.ramp.noOLR <- RAMP(X = X.3, y = y.3,
+                       penalty = "LASSO",
+                       tune = "BIC",
+                       n.lambda = 500)
+
+NE3.refit.noOLR <- refit_ramp(NE3.ramp.noOLR, X.3)
+
+#lm refit
+lm.data.3 <- as.data.frame(cbind(y.3, X.3))
+names(lm.data.3)[1] <- "co"
+
+NE3.lm.noOLR <- lm(formula(NE3.refit.noOLR), lm.data.3)
+
+summary(NE3.lm.noOLR)
+
+
+#SE Aus Early (Group 1)
+y.1 <- as.numeric(SE.resp[[1]]) #co response
+X.1 <- cbind(as.matrix(SE.pred[[1]][ ,c(1:52, 105:312)])) #preds without DMI and OLR
+
+SE1.ramp.noOLR <- RAMP(X = X.1, y = y.1,
+                       penalty = "LASSO",
+                       tune = "BIC",
+                       n.lambda = 500)
+
+SE1.refit.noOLR <- refit_ramp(SE1.ramp.noOLR, X.1)
+
+#lm refit
+lm.data.1 <- as.data.frame(cbind(y.1, X.1))
+names(lm.data.1)[1] <- "co"
+
+SE1.lm.noOLR <- lm(formula(SE1.refit.noOLR), lm.data.1)
+
+summary(SE1.lm.noOLR)
+
+
+#SE Aus Middle (Group 2)
+y.2 <- as.numeric(SE.resp[[2]]) #co response
+X.2 <- cbind(as.matrix(SE.pred[[2]][ ,c(1:52, 105:312)])) #preds without DMI and OLR
+
+SE2.ramp.noOLR <- RAMP(X = X.2, y = y.2,
+                 penalty = "LASSO",
+                 tune = "BIC",
+                 n.lambda = 500)
+
+SE2.refit.noOLR <- refit_ramp(SE2.ramp.noOLR, X.2)
+
+#lm refit
+lm.data.2 <- as.data.frame(cbind(y.2, X.2))
+names(lm.data.2)[1] <- "co"
+
+SE2.lm.noOLR <- lm(formula(SE2.refit.noOLR), lm.data.2)
+
+summary(SE2.lm.noOLR)
+
+
+#SE Aus Late (Group 3)
+y.3 <- as.numeric(SE.resp[[3]]) #co response
+X.3 <- cbind(as.matrix(SE.pred[[3]][ ,c(1:52, 105:312)])) #preds without DMI and OLR
+
+SE3.ramp.noOLR <- RAMP(X = X.3, y = y.3,
+                       penalty = "LASSO",
+                       tune = "BIC",
+                       n.lambda = 500)
+
+SE3.refit.noOLR <- refit_ramp(SE3.ramp.noOLR, X.3)
+
+#lm refit
+lm.data.3 <- as.data.frame(cbind(y.3, X.3))
+names(lm.data.3)[1] <- "co"
+
+SE3.lm.noOLR <- lm(formula(SE3.refit.noOLR), lm.data.3)
+
+summary(SE3.lm.noOLR)
+
+#output models
+NEmodels.noOLR <- list(NE1.lm.noOLR, NE2.lm.noOLR, NE3.lm.noOLR)
+NErefits.noOLR  <- list(NE1.refit.noOLR, NE2.refit.noOLR, NE3.refit.noOLR)
+SEmodels.noOLR  <- list(SE1.lm.noOLR, SE2.lm.noOLR, SE3.lm.noOLR)
+SErefits.noOLR  <- list(SE1.refit.noOLR, SE2.refit.noOLR, SE3.refit.noOLR)
+setwd("~/CO_AUS/AusCOmodeling/Data") 
+save(NEmodels.noOLR , SEmodels.noOLR , 
+     NErefits.noOLR , SErefits.noOLR , file = "noOLR_RAMPmodels.rda")
+
+
+## ---- LOYO Models ---- ##
+
+#Train/Test Setup
+
+## --- partial data setup 
+#leaving out a single year (loo)
+NE.pred.train <- list()
+NE.resp.train <- list()
+SE.pred.train <- list()
+SE.resp.train <- list()
+for (k in 1:length(seasons)) {
+  #NE Aus
+  NE.pred.train[[seasons[k]]] <- pred_setup(NEAus.lag, season.weeks, NE.early, NE.mid, NE.late, j = -c(k))
+  NE.resp.train[[seasons[k]]] <- resp_setup(NEresp.mat, season.weeks, NE.early, NE.mid, NE.late, j = -c(k)) 
+  
+  #SE Aus
+  SE.pred.train[[seasons[k]]] <- pred_setup(SEAus.lag, season.weeks, SE.early, SE.mid, SE.late, j = -c(k))
+  SE.resp.train[[seasons[k]]] <- resp_setup(SEresp.mat, season.weeks, SE.early, SE.mid, SE.late, j = -c(k)) 
+}
+
+#extracting a single year (validation data)
+NE.pred.valid <- list()
+NE.resp.valid <- list()
+SE.pred.valid <- list()
+SE.resp.valid <- list()
+for (k in 1:length(seasons)) {
+  #NE Aus
+  NE.pred.valid[[seasons[k]]] <- pred_setup(NEAus.lag, season.weeks, NE.early, NE.mid, NE.late, j = c(k))
+  NE.resp.valid[[seasons[k]]] <- resp_setup(NEresp.mat, season.weeks, NE.early, NE.mid, NE.late, j = c(k))
+  
+  #SE Aus
+  SE.pred.valid[[seasons[k]]] <- pred_setup(SEAus.lag, season.weeks, SE.early, SE.mid, SE.late, j = c(k))
+  SE.resp.valid[[seasons[k]]] <- resp_setup(SEresp.mat, season.weeks, SE.early, SE.mid, SE.late, j = c(k)) 
+}
+
+
+#models (refit and lm)
+NE.vary.terms <- NULL #non-fixed terms
+NE.const.LM <- NULL #fixed term model
+NE.vary.LM <- NULL #non-fixed term model
+for (i in 1:length(seasons)) {
+  #data w/o season (train)
+  train.resp <- NE.resp.train[[i]]
+  train.pred <- NE.pred.train[[i]]
+  
+  #data w/ season (test/validation)
+  valid.resp <- NE.resp.valid[[i]]
+  valid.pred <- NE.pred.valid[[i]]    
+  
+  #group data objects
+  NE.var.refit <- NULL #varying terms
+  NE.con <- NULL #constant linear models
+  NE.var <- NULL #varying linear models
+  
+  for (j in 1:3) {
+    #get base model terms (and fits)
+    NE.base.LM <- NEmodels[[j]] #lm model for NE group j 
+    NE.base.terms <- NErefits[[j]] #terms for NE group j
+  
+    #lm fit data setup
+    y.train <- as.numeric(train.resp[[j]])
+    #with OLR
+    X.train <- cbind(as.matrix(train.pred[[j]][ ,c(1:52, 105:364)])) 
+    
+    #varying ramp fit
+    vary.fit <- RAMP(X = X.train, y = y.train,
+                     penalty = "LASSO",
+                     tune = "BIC",
+                     n.lambda = 500)
+    #refit
+    NE.refit.vary <- refit_ramp(vary.fit, X.train)
+    
+    lm.data.fit <- as.data.frame(cbind(y.train, X.train))
+    names(lm.data.fit)[1] <- "co"
+    
+    #refit
+    NE.lm.const <- lm(formula(NE.base.terms), lm.data.fit)
+    NE.lm.vary <- lm(formula(NE.refit.vary), lm.data.fit)
+    
+    #assign terms and models
+    NE.var.refit[[j]] <- NE.refit.vary
+    NE.con[[j]] <- NE.lm.const
+    NE.var[[j]] <- NE.lm.vary
+    
+    #TODO: add in prediction and validation, as needed
+  }
+  NE.vary.terms[[seasons[i]]] <- NE.var.refit
+  NE.const.LM[[seasons[i]]] <- NE.con
+  NE.vary.LM[[seasons[i]]] <- NE.var
+}  
+
+
+
+SE.vary.terms <- NULL #non-fixed terms
+SE.const.LM <- NULL #fixed term model
+SE.vary.LM <- NULL #non-fixed term model
+for (i in 1:length(seasons)) {
+  #data w/o season (train)
+  train.resp <- SE.resp.train[[i]]
+  train.pred <- SE.pred.train[[i]]
+  
+  #data w/ season (test/validation)
+  valid.resp <- SE.resp.valid[[i]]
+  valid.pred <- SE.pred.valid[[i]]    
+  
+  #group data objects
+  SE.var.refit <- NULL #varying terms
+  SE.con <- NULL #constant linear models
+  SE.var <- NULL #varying linear models
+  
+  for (j in 1:3) {
+    #get base model terms (and fits)
+    SE.base.LM <- SEmodels[[j]] #lm model for NE group j 
+    SE.base.terms <- SErefits[[j]] #terms for NE group j
+    
+    #lm fit data setup
+    y.train <- as.numeric(train.resp[[j]])
+    #with OLR
+    X.train <- cbind(as.matrix(train.pred[[j]][ ,c(1:52, 105:364)])) 
+    
+    #varying ramp fit
+    vary.fit <- RAMP(X = X.train, y = y.train,
+                     penalty = "LASSO",
+                     tune = "BIC",
+                     n.lambda = 500)
+    #refit
+    SE.refit.vary <- refit_ramp(vary.fit, X.train)
+    
+    lm.data.fit <- as.data.frame(cbind(y.train, X.train))
+    names(lm.data.fit)[1] <- "co"
+    
+    #refit
+    SE.lm.const <- lm(formula(SE.base.terms), lm.data.fit)
+    SE.lm.vary <- lm(formula(SE.refit.vary), lm.data.fit)
+    
+    #assign terms and models
+    SE.var.refit[[j]] <- SE.refit.vary
+    SE.con[[j]] <- SE.lm.const
+    SE.var[[j]] <- SE.lm.vary
+    
+    #TODO: add in prediction and validation, as needed
+  }
+  SE.vary.terms[[seasons[i]]] <- SE.var.refit
+  SE.const.LM[[seasons[i]]] <- SE.con
+  SE.vary.LM[[seasons[i]]] <- SE.var
+}  
+
+#output models
+NEmodels.loyo <- list(NE.vary.terms, NE.const.LM, NE.vary.LM)
+SEmodels.loyo <- list(SE.vary.terms, SE.const.LM, SE.vary.LM)
+setwd("~/CO_AUS/AusCOmodeling/Data") 
+save(NEmodels.loyo, SEmodels.loyo, file = "loyo_models.rda")
 
 
