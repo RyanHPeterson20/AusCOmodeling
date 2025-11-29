8591d34c9dd5075c79199a6e7956a139d3ab520e
Ryan Peterson <rhpeterson@mines.edu>
2025-11-28 22:45:09 -0700

    update

--- Figures/ts_figures.R
+++ Figures/ts_figures.R
@@ -30,17 +30,228 @@ top.col.resp <- "#C71C1C"
 bot.col.resp <- "#2A5674FF"
 
 #predictor colors
-top.col.pred <- "#EF6A4CFF"
-bot.col.pred <- "#4F90A6FF"
-
+top.col.pred <- "#F2855DFF"
+bot.col.pred <- "#68ABB8FF"
 
 ## response ts ##
 resp.raw <- resp.raw[resp.raw$date <= "2021-01-06", ]
 #resp.raw <- resp.raw[resp.raw$year <= 2020, ]
 
+#predictor time series
+#TODO: update with aligned years, 2001 to end of 2020 (2001-01-03 to 2021-01-06)
+pred.raw <- pred.raw[pred.raw$date >= "2001-01-03" & pred.raw$date <= "2021-01-06", ]
+#pred.raw <- pred.raw[pred.raw$date <= "2021-01-06", ] #old version, keep until figure is finalized
+
 ## response anoms
 NE.anom <- resp.raw$NEAus.anom
 SE.anom <- resp.raw$SEAus.anom
+
+## predictor anoms (std)
+nino.anom.std <- scale(pred.raw$nino.anom)
+wtio.anom.std <- scale(pred.raw$wtio.anom)
+etio.anom.std <- scale(pred.raw$etio.anom)
+tsa.anom.std <- scale(pred.raw$tsa.anom)
+aao.anom.std <- scale(pred.raw$aao.anom)
+olr.anom.std <- scale(pred.raw$olr.anom)
+
+## time setup ##
+#resp:
+resp.time <- resp.raw$date
+resp.week <- resp.raw$week
+resp.time.range <- range(resp.time)
+
+#yearly ticks
+x.ticks.resp <- seq(year(resp.time.range[1]), year(resp.time.range[2]), by = 1)
+x.ticks.resp <- ymd(paste0(x.ticks.resp, "01", "01"))
+x.resp.reduced <- x.ticks.resp[1:20]
+
+time.resp.plot <- as.Date(resp.time)
+
+#preds:
+pred.time <- pred.raw$date
+pred.week <- pred.raw$week
+pred.time.range <- range(pred.time)
+
+#yearly ticks
+x.ticks.pred <- seq(year(pred.time.range[1]), year(pred.time.range[2]), by = 1)
+x.ticks.pred <- ymd(paste0(x.ticks.pred, "01", "01"))
+x.pred.reduced <- x.ticks.pred[1:20]
+
+time.pred.plot <- as.Date(pred.time)
+
+#updated figure 1 time series, 
+## resp SE Aus
+## pred Nino, WTIO, ETIO, TSA, SAM, OLR
+
+#updated y.ticks
+#response setup:
+y.SE.max <-  round(max(SE.anom)/10)*10
+y.SE.min <- round(min(SE.anom)/10)*10
+y.SE.ticks <- c(-20, 0, 25, 50)
+#y.SE.ticks <- seq(y.SE.min, y.SE.max, by = 20)
+
+#predictors setup:
+y.nino.max <- max(round(range(nino.anom.std)))
+y.nino.ticks <- seq(-y.nino.max, y.nino.max, by = 1)
+
+y.wtio.max <- max(round(range(wtio.anom.std)))
+y.wtio.ticks <- seq(-y.wtio.max, y.wtio.max, by = 1)
+
+#uses abs max
+y.etio.max <- max(abs(round(range(etio.anom.std))))
+y.etio.ticks <- seq(-y.etio.max, y.etio.max, by = 1)
+
+y.tsa.max <- max(round(range(tsa.anom.std)))
+y.tsa.ticks <- seq(-y.tsa.max, y.tsa.max, by = 1)
+
+#uses abs max
+y.aao.max <- max(abs(round(range(aao.anom.std))))
+y.aao.ticks <- seq(-y.aao.max, y.aao.max, by = 1)
+
+y.olr.max <- max(abs(round(range(olr.anom.std))))
+y.olr.ticks <- seq(-y.olr.max, y.olr.max, by = 1)
+
+#finalize preds with common range
+y.tick.max <- max(y.nino.max, y.wtio.max, y.etio.max, y.tsa.max, y.aao.max)
+y.tick.steps <- y.tick.max/2
+y.tick.seq <- seq(y.tick.steps, y.tick.max-y.tick.steps, by = y.tick.steps)
+y.tick.lab <- c(-rev(y.tick.seq), 0, y.tick.seq)
+
+
+#envelope plot setup
+#resp
+#SE Aus:
+over.seaus <- SE.anom >= 0
+seaus.top <- SE.anom
+seaus.top[!over.seaus] <- 0
+seaus.bot <- SE.anom
+seaus.bot[over.seaus] <- 0
+
+#preds
+#nino:
+over.nino <- nino.anom.std >= 0
+nino.top <- nino.anom.std
+nino.top[!over.nino] <- 0
+nino.bot <- nino.anom.std
+nino.bot[over.nino] <-0
+#wtio:
+over.wtio <- wtio.anom.std >= 0
+wtio.top <- wtio.anom.std
+wtio.top[!over.wtio] <- 0
+wtio.bot <- wtio.anom.std
+wtio.bot[over.wtio] <-0
+#etio:
+over.etio <- etio.anom.std >= 0
+etio.top <- etio.anom.std
+etio.top[!over.etio] <- 0
+etio.bot <- etio.anom.std
+etio.bot[over.etio] <-0
+#tsa:
+over.tsa <- tsa.anom.std >= 0
+tsa.top <- tsa.anom.std
+tsa.top[!over.tsa] <- 0
+tsa.bot <- tsa.anom.std
+tsa.bot[over.tsa] <-0
+#aao
+over.aao <- aao.anom.std >= 0
+aao.top <- aao.anom.std
+aao.top[!over.aao] <- 0
+aao.bot <- aao.anom.std
+aao.bot[over.aao] <-0
+#olr
+over.olr <- olr.anom.std >= 0
+olr.top <- olr.anom.std
+olr.top[!over.olr] <- 0
+olr.bot <- olr.anom.std
+olr.bot[over.olr] <-0
+
+
+par(mfrow = c(2, 1))
+
+#par(mfrow = c(7, 1))
+#response data figure
+#SE Aus sub-figure
+plot(time.resp.plot, SE.anom, type = "l", col = "black", lwd = 2,
+     xaxt = "n", xlab = "",
+     yaxt = "n", ylab = "CO Anomaly [ppb]", col.lab = "black",
+     xlim = c(as.Date(resp.time.range[1]) + months(7), as.Date(resp.time.range[2]) - months(7)),
+     ylim = range(y.SE.ticks), bty = "n", cex.lab = 1.75,  xpd = NA)
+axis(side = 2, at = y.SE.ticks, cex.axis = 1.67, 
+     col = NA, line = 0.5,
+     col.ticks = "black", col.axis = "black", las =1)
+abline(v = x.ticks.resp[1:(length(x.ticks.resp))],
+       lty = 2, col = "grey", lwd = 2)
+abline(h = 0, lty = 1, col = "grey", lwd = 1)
+envelopePlot(x1 = time.resp.plot,
+             y1 = seaus.top,
+             x2 = time.resp.plot,
+             y2 = rep(0, length(seaus.top)),
+             col = alpha(top.col.resp, 0.67),
+             lineCol = NA)
+envelopePlot(x1 = time.resp.plot,
+             y1 = seaus.bot,
+             x2 = time.resp.plot,
+             y2 = rep(0, length(seaus.bot)),
+             col = alpha(bot.col.resp, 0.67),
+             lineCol = NA)
+legend(x = c(ymd("2000-11-01"), ymd("2002-01-01")), #adjust according to final .png output
+       y = c(45,40),
+       legend = "SE Aus",
+       box.col = NA, bg = NA,
+       xpd = NA, text.col = "grey40", cex = 2.5)
+
+#predictor data figures
+#TODO: fill in-between
+#nino sub-figure
+
+#wtio sub-figure
+
+#etio sub-figure
+
+#tsa sub-figure
+
+#aao (sam) sub-figure
+
+#olr sub-figure
+plot(time.pred.plot, olr.anom.std, type = "l", col = "black", lwd = 2,
+     xaxt = "n", xlab = "",
+     yaxt = "n", ylab = "Anomaly [W/m^2]", col.lab = "black",
+     xlim = c(as.Date(pred.time.range[1]) + months(7), as.Date(pred.time.range[2]) - months(7)),
+     ylim = range(y.olr.ticks), bty = "n", cex.lab = 1.75,  xpd = NA)
+axis(side = 2, at = y.tick.lab, cex.axis = 1.67, 
+     col = NA, line = 0.5,
+     col.ticks = "black", col.axis = "black", las =1)
+abline(v = x.ticks.pred[1:(length(x.ticks.pred))],
+       lty = 2, col = "grey", lwd = 2)
+abline(h = 0, lty = 1, col = "grey", lwd = 1)
+envelopePlot(x1 = time.pred.plot,
+             y1 = olr.top,
+             x2 = time.pred.plot,
+             y2 = rep(0, length(olr.top)),
+             col = alpha(top.col.pred, 0.67),
+             lineCol = NA)
+envelopePlot(x1 = time.pred.plot,
+             y1 = olr.bot,
+             x2 = time.pred.plot,
+             y2 = rep(0, length(olr.bot)),
+             col = alpha(bot.col.pred, 0.67),
+             lineCol = NA)
+legend(x = c(ymd("2000-11-01"), ymd("2002-01-01")),
+       y = c(4, 4),
+       legend = "OLR",
+       box.col = NA, bg = NA,
+       xpd = NA, text.col = "grey40", cex = 2.75)
+
+text(x = x.pred.reduced + months(6),
+     y = range(y.olr.ticks)[1]-0.5,
+     labels = year(x.pred.reduced),
+     cex = 2.25, col = "black", xpd = NA)
+
+dev.off()
+
+
+
+#TODO: previous (old) figures, move everything below to a new file (e.g. SI ts figures) or delete
 
 #test scale anoms
 test.for.zero(scale(NE.anom), (NE.anom - mean(NE.anom))/sd(NE.anom))
@@ -50,21 +261,18 @@ test.for.zero(scale(SE.anom), (SE.anom - mean(SE.anom))/sd(SE.anom))
 NE.anom.std <- scale(NE.anom)
 SE.anom.std <- scale(SE.anom)
 
-#temporal setup
+#time setup
 resp.time <- resp.raw$date
 resp.week <- resp.raw$week
 
 resp.time.range <- range(resp.time)
-
 
 #yearly ticks
 x.ticks <- seq(year(resp.time.range[1]), year(resp.time.range[2]), by = 1)
 x.ticks <- ymd(paste0(x.ticks, "01", "01"))
 x.ticks.reduced <- x.ticks[1:20]
 
-#TODO: expand to both regions when single figure is finalized
 #figure test for a single region
-
 time.plot <- as.Date(resp.time)
 
 #get y.ticks and labels
@@ -406,7 +614,6 @@ olr.bot <- olr.anom.std
 olr.bot[over.olr] <-0
 
 
-#TODO: add olr
 
 #individual plots
 setwd("~/CO_AUS/AusCOmodeling/Figures")
