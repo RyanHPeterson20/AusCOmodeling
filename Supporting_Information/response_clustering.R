5ad7cd4970b638108cf6ce7c6e8c19b9979f2bee
Ryan Peterson <rhpeterson@mines.edu>
2026-02-18 00:42:29 -0700

    updates

--- Supporting_Information/response_clustering.R
+++ Supporting_Information/response_clustering.R
@@ -49,22 +49,29 @@ SEhc.ward$order <- c(1:29)
 
 #figures
 #TODO: update figures to align with paper/SI style
-#setwd("~/CO_AUS/...")
-#png(filename = "NEdend_base.png", width = 3600, height = 1200, res = 300)
-stats:::plot.hclust(NEhc.ward,  hang = -1, labels = FALSE, bty = "n", sub = "", ylab = "",
-                    xlab = "Week", axes = FALSE, cex.main = 1.1,
-                    cex.lab = 1.1)
-title("NE Aus : Response Clustering", adj = 0)
+setwd("~/CO_AUS/AusCOmodeling/Supporting_Information/SI_Figures")
+png(filename = "NEclust_dend.png", width = 3600, height = 1200, res = 300)
+par(mar = c(5, 5, 4, 0))
+stats:::plot.hclust(NEhc.ward,  hang = -1, labels = FALSE, bty = "n", sub = "", ylab = "Height",
+                    xlab = "Week", axes = FALSE, cex.lab = 1.25, main = "", lwd = 1.5)
+title("NE Aus : Response Cluster Dendrogram", adj = 0, cex.main = 1.33)
 axis(2)
 axis(1, at = 1:29, labels = c(season.weeks[c(1:29)]), 
      las = 3, gap.axis = 0, tck = 0.5, lty = 0, cex.axis = 1.1)
+rect(xleft = 0.5, xright = 9.5, ybottom = 0, ytop = 67, col = rgb(1, 0, 0, 0.22), border = NA)
+rect(xleft = 9.5, xright = 14.5, ybottom = 0, ytop = 67, col = rgb(0, 1, 0, 0.22), border = NA)
+rect(xleft = 14.5, xright = 29.5, ybottom = 0, ytop = 67, col = rgb(0, 0, 1, 0.22), border = NA)
+text(5, -5, "Early", col = "black", cex = 1.1, xpd = TRUE)
+text(12, -5, "Peak", col = "black", cex = 1.1, xpd = TRUE)
+text(22, -5, "Late", col = "black", cex = 1.1, xpd = TRUE)
+dev.off()
 
 
 setwd("~/CO_AUS/AusCOmodeling/Supporting_Information/SI_Figures")
 png(filename = "SEclust_dend.png", width = 3600, height = 1200, res = 300)
 par(mar = c(5, 5, 4, 0))
 stats:::plot.hclust(SEhc.ward,  hang = -1, labels = FALSE, bty = "n", sub = "", ylab = "Height",
-                    xlab = "Week", axes = FALSE, cex.lab = 1.25, main = "")
+                    xlab = "Week", axes = FALSE, cex.lab = 1.25, main = "", lwd = 1.5)
 title("SE Aus : Response Cluster Dendrogram", adj = 0, cex.main = 1.33)
 axis(2)
 axis(1, at = 1:29, labels = c(season.weeks[c(1:29)]), 
@@ -72,9 +79,9 @@ axis(1, at = 1:29, labels = c(season.weeks[c(1:29)]),
 rect(xleft = 0.5, xright = 13.5, ybottom = 0, ytop = 70, col = rgb(1, 0, 0, 0.22), border = NA)
 rect(xleft = 13.5, xright = 17.5, ybottom = 0, ytop = 70, col = rgb(0, 1, 0, 0.22), border = NA)
 rect(xleft = 17.5, xright = 29.5, ybottom = 0, ytop = 70, col = rgb(0, 0, 1, 0.22), border = NA)
-text(7, -5.6, "Early", col = "black", cex = 1.1, xpd = TRUE)
-text(15.5, -5.6, "Peak", col = "black", cex = 1.1, xpd = TRUE)
-text(23.5, -5.6, "Late", col = "black", cex = 1.1, xpd = TRUE)
+text(7, -5, "Early", col = "black", cex = 1.1, xpd = TRUE)
+text(15.5, -5, "Peak", col = "black", cex = 1.1, xpd = TRUE)
+text(23.5, -5, "Late", col = "black", cex = 1.1, xpd = TRUE)
 dev.off()
 
 
