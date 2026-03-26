e8265ac2d7906946c2df38521309e5079e8fc33e
Ryan Peterson <rhpeterson@mines.edu>
2026-03-26 16:25:01 -0600

    updates

--- Supporting_Information/pIOD/pIOD_index.R
+++ Supporting_Information/pIOD/pIOD_index.R
@@ -51,34 +51,80 @@ son.div.full <-  c(0.5, son.div, n.son+0.5)
 
 year.loc.son <- (head(son.div.full, -1) + tail(son.div.full, -1))/2
 
+#cor setup
+#get correlation values and plots
+wtio.son.cor <- cor(wtio.son, m.index.son)
+etio.son.cor <- cor(etio.son, s.index.son)
 
-#figures
+#get 2019 indices
+ind.2019 <- which(pIOD.df$year == 2019)
+#ind.2019 <- which(son.pred.df$year == 2019)
+
+
+#full range from strong and moderate indices and manually adjusted range.
+full.range <- range(strong.range, mod.range)
+full.range.manual <- c(-3.5, 5.25) 
+
+#cex setup
+line.width <- 1.75 #primary ts lines
+subtitle.cex <- 1.5
+
+#etio figures
+
+
+#SI figure a 
 setwd("~/CO_AUS/AusCOmodeling/Supporting_Information/SI_Figures")
-png(filename = "SON_strong_etio.png", width = 3000, height = 1500, res = 250) #TODO update at res 300
-par(mar = c(4.5, 4.5, 3, 7.65))
+png(filename = "SI_fig3a_strongetio_ts.png", width = 3600, height = 2000, res = 275) 
+par(mar = c(4.2, 4.25, 3, 1))
 plot(1:length(s.index.son), s.index.son, type = "l",  col = "firebrick", 
-     ylim=strong.range, c(11, 263), 
-     ylab = "Index", xlab = "Year", cex.lab = 1.15, lwd = 1.5,
+     ylim = full.range.manual, xlim = c(11, 263), cex = 1.25,
+     ylab = "Index", xlab = "Year", cex.lab = 1.25, lwd = line.width,
      axes = FALSE)
-axis(1, at = year.loc.son, labels = years.pIOD, tick = FALSE, cex.axis = 0.85)
-axis(2)                      
+axis(1, at = year.loc.son, labels = years.pIOD, tick = FALSE, cex.axis = 1.05)
+axis(2, at = seq(-2,4, by = 2), cex.axis = 1.2)
+title("(a) Weekly S-Index and ETIO", adj = 0, cex.main = subtitle.cex)
 box()
-lines(1:length(etio.son), scale(-etio.son), col = "royalblue4", lwd = 1.5)
-abline(h = 0, lty = 2, lwd = 0.95)
-abline(v=son.div.full, lty = 3, lwd = 0.95, col = "gray30")
-title("SON Weekly : S-Index & ETIO", adj = 0, cex = 1.1)
-legend("topright",
+lines(1:length(etio.son), scale(-etio.son), col = "royalblue4", lwd = line.width)
+abline(h = 0, lty = 2, lwd = 0.95, col = "gray23")
+abline(v=son.div.full, lty = 1, lwd = 0.75, col = "gray45")
+
+#TODO: move inside (bottom-right)
+legend("bottomright",
        legend = c("S-Index", "-ETIO"),
-       lty    = c(1,1),                 # line type
-       lwd    = 1.5,                 # line width
+       lty    = c(1, 1),                 # line type
+       lwd    = line.width,                 # line width
        col    = c( "firebrick", "royalblue4"),
        #bty    = "n",               # no box; remove if you want a box
-       inset  = c(-0.152, 0 ),
-       cex = 1.1, 
+       #inset  = c(-0.152, 0 ),
+       cex = 1.25, 
        xpd = TRUE)
 dev.off()
 
 
+png(filename = "SI_fig3b_strongetio_cor.png", width = 2000, height = 2000, res = 275)
+par(mar = c(4.2, 4.25, 3, 1))
+plot(etio.son, s.index.son, pch = 19, cex = 1.25,
+     xlab = "ETIO", ylab = "S-Index",
+     ylim = full.range.manual,
+     cex.lab = 1.25, 
+     axes = FALSE)
+axis(1, cex.axis = 1.2)
+axis(2, at = seq(-2,4, by = 2), cex.axis = 1.2)
+title("(b) Correlation Weekly S-Index and ETIO", adj = 0, cex = subtitle.cex)
+box()
+points(etio.son[ind.2019], s.index.son[ind.2019], pch = 19, cex = 1.25, col = "firebrick")
+#text(-1.75, -1.5, paste0("Cor = ", round(etio.son.cor, 3)), adj = 0 )
+legend("bottomleft",
+       legend = c("2019"),
+       pch = 19,
+       col = "firebrick",
+       #bty    = "n",               # no box; remove if you want a box
+       cex = 1.25, 
+       xpd = TRUE)
+dev.off()
+
+
+#TODO: finish up here....
 setwd("~/CO_AUS/AusCOmodeling/Supporting_Information/SI_Figures")
 png(filename = "SON_mod_wtio.png", width = 3000, height = 1500, res = 250)
 par(mar = c(4.5, 4.5, 3, 7.65))
@@ -107,17 +153,34 @@ dev.off()
 
 
 
-#get correlation values and plots
-wtio.son.cor <- cor(wtio.son, m.index.son)
-etio.son.cor <- cor(etio.son, s.index.son)
+png(filename = "SI_fig3d_modwtio_cor.png", width = 2000, height = 2000, res = 275)
+par(mar = c(4.5, 4.25, 3, 1))
+plot(wtio.son, m.index.son, pch = 19, cex = 0.89,
+     xlab = "WTIO", ylab = "M-Index",
+     cex.lab = 1.15, 
+     axes = FALSE)
+axis(1, cex.axis = 1.05)
+axis(2, cex.axis = 1.05) 
+title("SON: WTIO and M-Index", adj = 0, cex.main = subtitle.cex)
+box()
+points(wtio.son[ind.2019], m.index.son[ind.2019], pch = 19, cex = 0.89, col = "firebrick")
+#text(-0.45, 2, paste0("Cor = ", round(wtio.son.cor, 3)), adj = 0 )
 
-#get 2019 indices
-ind.2019 <- which(pIOD.df$year == 2019)
-#ind.2019 <- which(son.pred.df$year == 2019)
+legend("bottomright",
+       legend = c("2019"),
+       pch = 19,
+       col = "firebrick",
+       #bty    = "n",               # no box; remove if you want a box
+       cex = 0.89, 
+       xpd = TRUE)
+dev.off()
 
 
 
-setwd("~/CO_AUS/AusCOmodeling/Figures")
+
+#correlation figures
+
+setwd("~/CO_AUS/AusCOmodeling/Supporting_Information/SI_Figures")
 
 png(filename = "wtio_mindexSON.png", width = 2000, height = 1200, res = 250)
 par(mar = c(4.25, 4.25, 3, 2))
@@ -140,25 +203,4 @@ legend("bottomright",
        xpd = TRUE)
 dev.off()
 
-
-png(filename = "etio_sindexSON.png", width = 2000, height = 1200, res = 250)
-par(mar = c(4.25, 4.25, 3, 2))
-plot(etio.son, s.index.son, pch = 19, cex = 0.89,
-     xlab = "ETIO", ylab = "S-Index",
-     cex.lab = 1.15, 
-     axes = FALSE)
-axis(1, cex.axis = 1.05)
-axis(2, cex.axis = 1.05)                      
-box()
-points(etio.son[ind.2019], s.index.son[ind.2019], pch = 19, cex = 0.89, col = "firebrick")
-#text(-1.75, -1.5, paste0("Cor = ", round(etio.son.cor, 3)), adj = 0 )
-title("SON: ETIO and S-Index", adj = 0, cex = 1.1)
-legend("topright",
-       legend = c("2019"),
-       pch = 19,
-       col = "firebrick",
-       #bty    = "n",               # no box; remove if you want a box
-       cex = 0.89, 
-       xpd = TRUE)
-dev.off()
 
