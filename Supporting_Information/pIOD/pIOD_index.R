05e8bf960a22b8a46f41cd945e12ea74c023bb7e
Ryan Peterson <rhpeterson@mines.edu>
2026-03-31 10:46:38 -0600

    updates

--- Supporting_Information/pIOD/pIOD_index.R
+++ Supporting_Information/pIOD/pIOD_index.R
@@ -63,32 +63,38 @@ ind.2019 <- which(pIOD.df$year == 2019)
 
 #full range from strong and moderate indices and manually adjusted range.
 full.range <- range(strong.range, mod.range)
-full.range.manual <- c(-3.5, 5.25) 
+full.range.manual <- c(-3.75, 5.25) 
 
 #cex setup
-line.width <- 1.75 #primary ts lines
-subtitle.cex <- 1.5
+line.width <- 2.05 #primary ts lines
+subtitle.cex <- 1.85
+ylab.cex <- 1.75 
+yaxis.cex <- 1.55
+xlab.cex <- 1.75
 
 #etio figures
 
 
 #SI figure a 
 setwd("~/CO_AUS/AusCOmodeling/Supporting_Information/SI_Figures")
+
 png(filename = "SI_fig3a_strongetio_ts.png", width = 3600, height = 2000, res = 275) 
 par(mar = c(4.2, 4.25, 3, 1))
 plot(1:length(s.index.son), s.index.son, type = "l",  col = "firebrick", 
      ylim = full.range.manual, xlim = c(11, 263), cex = 1.25,
-     ylab = "Index", xlab = "Year", cex.lab = 1.25, lwd = line.width,
+     ylab = "", xlab = "", lwd = line.width, lty = 1, 
      axes = FALSE)
-axis(1, at = year.loc.son, labels = years.pIOD, tick = FALSE, cex.axis = 1.05)
-axis(2, at = seq(-2,4, by = 2), cex.axis = 1.2)
+axis(1, at = year.loc.son, labels = years.pIOD, tick = FALSE, cex.axis = 1.10)
+axis(2, at = seq(-2,4, by = 2), cex.axis = yaxis.cex, las = 1)
 title("(a) Weekly S-Index and ETIO", adj = 0, cex.main = subtitle.cex)
+mtext("Index", side = 2, cex = ylab.cex, line = 2.75) #y-axis
+mtext("Year", side = 1, cex = xlab.cex, line = 2.75) #x-axis
 box()
-lines(1:length(etio.son), scale(-etio.son), col = "royalblue4", lwd = line.width)
-abline(h = 0, lty = 2, lwd = 0.95, col = "gray23")
+lines(1:length(etio.son), scale(-etio.son), col = "royalblue4", lwd = line.width, lty = 1)
+abline(h = 0, lty = 1, lwd = 0.65, col = "gray23")
 abline(v=son.div.full, lty = 1, lwd = 0.75, col = "gray45")
 
-#TODO: move inside (bottom-right)
+#bottom-right legend
 legend("bottomright",
        legend = c("S-Index", "-ETIO"),
        lty    = c(1, 1),                 # line type
@@ -96,111 +102,88 @@ legend("bottomright",
        col    = c( "firebrick", "royalblue4"),
        #bty    = "n",               # no box; remove if you want a box
        #inset  = c(-0.152, 0 ),
-       cex = 1.25, 
+       cex = 1.5, 
        xpd = TRUE)
 dev.off()
 
 
 png(filename = "SI_fig3b_strongetio_cor.png", width = 2000, height = 2000, res = 275)
 par(mar = c(4.2, 4.25, 3, 1))
-plot(etio.son, s.index.son, pch = 19, cex = 1.25,
-     xlab = "ETIO", ylab = "S-Index",
-     ylim = full.range.manual,
-     cex.lab = 1.25, 
+plot(etio.son, s.index.son, pch = 19, cex = 1.25, 
+     xlab = "", ylab = "",
+     ylim = full.range.manual, 
      axes = FALSE)
-axis(1, cex.axis = 1.2)
-axis(2, at = seq(-2,4, by = 2), cex.axis = 1.2)
-title("(b) Correlation Weekly S-Index and ETIO", adj = 0, cex = subtitle.cex)
+axis(1, cex.axis = yaxis.cex)
+axis(2, at = seq(-2,4, by = 2), cex.axis = yaxis.cex, las = 1)
+title("(b) Correlation Weekly S-Index and ETIO", adj = 0, cex.main = subtitle.cex)
+mtext("S-Index", side = 2, cex = ylab.cex, line = 2.75) #y-axis
+mtext("ETIO", side = 1, cex = xlab.cex, line = 2.75) #x-axis
 box()
-points(etio.son[ind.2019], s.index.son[ind.2019], pch = 19, cex = 1.25, col = "firebrick")
+points(etio.son[ind.2019], s.index.son[ind.2019], pch = 23, cex = 1.5, col = "gray3", bg = "red1", lwd = 1.25)
 #text(-1.75, -1.5, paste0("Cor = ", round(etio.son.cor, 3)), adj = 0 )
 legend("bottomleft",
        legend = c("2019"),
-       pch = 19,
-       col = "firebrick",
+       pch = 23,
+       col = "gray3", pt.bg = "red1", pt.lwd = 1.25,
+       pt.cex = 1.5,
        #bty    = "n",               # no box; remove if you want a box
-       cex = 1.25, 
+       cex = 1.65, 
        xpd = TRUE)
 dev.off()
 
 
-#TODO: finish up here....
-setwd("~/CO_AUS/AusCOmodeling/Supporting_Information/SI_Figures")
-png(filename = "SON_mod_wtio.png", width = 3000, height = 1500, res = 250)
-par(mar = c(4.5, 4.5, 3, 7.65))
+
+#wtio figures
+
+png(filename = "SI_fig3c_modwtio_ts.png", width = 3600, height = 2000, res = 275) 
+par(mar = c(4.2, 4.25, 3, 1))
 plot(1:length(m.index.son), m.index.son, type = "l",  col = "forestgreen", 
-     ylim=mod.range, xlim = c(11, 263), 
-     ylab = "Index", xlab = "Year", cex.lab = 1.15, lwd = 1.5,
+     ylim = full.range.manual, xlim = c(11, 263), cex = 1.25,
+     ylab = "", xlab = "", lwd = line.width, lty = 1, 
      axes = FALSE)
-axis(1, at = year.loc.son, labels = years.pIOD, tick = FALSE, cex.axis = 0.85)
-axis(2)                      
+axis(1, at = year.loc.son, labels = years.pIOD, tick = FALSE, cex.axis = 1.10)
+axis(2, at = seq(-2,4, by = 2), cex.axis = yaxis.cex, las = 1)
+title("(a) Weekly M-Index and WTIO", adj = 0, cex.main = subtitle.cex)
+mtext("Index", side = 2, cex = ylab.cex, line = 2.75) #y-axis
+mtext("Year", side = 1, cex = xlab.cex, line = 2.75) #x-axis
 box()
-lines(1:length(wtio.son), wtio.son, col = "magenta4",  lwd = 1.5)
-abline(h = 0, lty = 2, lwd = 0.95)
-abline(v=son.div.full, lty = 3, lwd = 0.95, col = "gray30")
-title("SON Weekly : M-Index & WTIO", adj = 0, cex = 1.1)
+lines(1:length(wtio.son), scale(wtio.son), col = "magenta4", lwd = line.width, lty = 1)
+abline(h = 0, lty = 1, lwd = 0.65, col = "gray23")
+abline(v=son.div.full, lty = 1, lwd = 0.75, col = "gray45")
+#top-right legend
 legend("topright",
        legend = c("M-Index", "WTIO"),
-       lty    = c(1,1),                 # line type
-       lwd    = 1.5,                 # line width
+       lty    = c(1, 1),                 # line type
+       lwd    = line.width,                 # line width
        col    = c( "forestgreen", "magenta4"),
        #bty    = "n",               # no box; remove if you want a box
-       inset  = c(-0.152, 0 ),
-       cex = 1.1, 
+       #inset  = c(-0.152, 0 ),
+       cex = 1.5, 
        xpd = TRUE)
+
 dev.off()
-
-
 
 
 png(filename = "SI_fig3d_modwtio_cor.png", width = 2000, height = 2000, res = 275)
 par(mar = c(4.5, 4.25, 3, 1))
-plot(wtio.son, m.index.son, pch = 19, cex = 0.89,
-     xlab = "WTIO", ylab = "M-Index",
-     cex.lab = 1.15, 
+plot(wtio.son, m.index.son, pch = 19, cex = 1.25, 
+     xlab = "", ylab = "",
+     ylim = full.range.manual, 
      axes = FALSE)
-axis(1, cex.axis = 1.05)
-axis(2, cex.axis = 1.05) 
-title("SON: WTIO and M-Index", adj = 0, cex.main = subtitle.cex)
+axis(1, cex.axis = yaxis.cex)
+axis(2, at = seq(-2,4, by = 2), cex.axis = yaxis.cex, las = 1)
+title("(b) Correlation Weekly M-Index and WTIO", adj = 0, cex.main = subtitle.cex)
+mtext("M-Index", side = 2, cex = ylab.cex, line = 2.75) #y-axis
+mtext("WTIO", side = 1, cex = xlab.cex, line = 2.75) #x-axis
 box()
-points(wtio.son[ind.2019], m.index.son[ind.2019], pch = 19, cex = 0.89, col = "firebrick")
-#text(-0.45, 2, paste0("Cor = ", round(wtio.son.cor, 3)), adj = 0 )
-
-legend("bottomright",
+points(wtio.son[ind.2019], m.index.son[ind.2019], pch = 23, cex = 1.5, col = "gray23", bg = "red1", lwd = 1.25)
+legend("topleft",
        legend = c("2019"),
-       pch = 19,
-       col = "firebrick",
+       pch = 23,
+       col = "gray23", pt.bg = "red1", pt.lwd = 1.25,
+       pt.cex = 1.5,
        #bty    = "n",               # no box; remove if you want a box
-       cex = 0.89, 
+       cex = 1.65, 
        xpd = TRUE)
 dev.off()
-
-
-
-
-#correlation figures
-
-setwd("~/CO_AUS/AusCOmodeling/Supporting_Information/SI_Figures")
-
-png(filename = "wtio_mindexSON.png", width = 2000, height = 1200, res = 250)
-par(mar = c(4.25, 4.25, 3, 2))
-plot(wtio.son, m.index.son, pch = 19, cex = 0.89,
-     xlab = "WTIO", ylab = "M-Index",
-     cex.lab = 1.15, 
-     axes = FALSE)
-axis(1, cex.axis = 1.05)
-axis(2, cex.axis = 1.05)                      
-box()
-points(wtio.son[ind.2019], m.index.son[ind.2019], pch = 19, cex = 0.89, col = "firebrick")
-#text(-0.45, 2, paste0("Cor = ", round(wtio.son.cor, 3)), adj = 0 )
-title("SON: WTIO and M-Index", adj = 0, cex = 1.1)
-legend("bottomright",
-       legend = c("2019"),
-       pch = 19,
-       col = "firebrick",
-       #bty    = "n",               # no box; remove if you want a box
-       cex = 0.89, 
-       xpd = TRUE)
-dev.off()
-
 
